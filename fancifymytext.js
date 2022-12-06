9655401c8aadfa17d71ca2d075c7e1e52ce238fb
Gary Shatraw Jr <92950166+GShatrawJr@users.noreply.github.com>
2022-12-06 14:13:27 -0800

    Add files via upload

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,14 +1,11 @@
 var textarea=document.getElementById("textarea");
-function Moo(){
-    textarea.style.textTransform="uppercase"
-    let str=textarea.value.split(".");
-    let str2=str.join("-Moo");
-    textarea.value=str2;
-}
 
-function makeBig(){
-    textarea.style.fontSize="24pt";
-}
+document.getElementsByName("fancify")[1].addEventListener("change",function(){
+    textarea.style.fontWeight="normal";
+    textarea.style.color="black";
+    textarea.style.textDecoration="none"
+    alert("Bo-ring!");
+})
 
 document.getElementsByName("fancify")[0].addEventListener("change",function(){
     textarea.style.fontWeight="bold";
@@ -17,9 +14,15 @@ document.getElementsByName("fancify")[0].addEventListener("change",function(){
     alert("So fancy!");
 })
 
-document.getElementsByName("fancify")[1].addEventListener("change",function(){
-    textarea.style.fontWeight="normal";
-    textarea.style.color="black";
-    textarea.style.textDecoration="none"
-    alert("Bo-ring!");
-})
+function Moo(){
+    textarea.style.textTransform="uppercase"
+    let str=textarea.value.split(".");
+    let str2=str.join("-Moo");
+    textarea.value=str2;
+    alert("The cow goes: MOO!")
+}
+
+function Big(){
+    textarea.style.fontSize="24pt";
+    alert("BIGGER!")
+}
